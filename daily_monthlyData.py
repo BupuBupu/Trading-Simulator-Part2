5767067f5fb6686a9dfa516de486e6fb0e51675f
Vipul-Vaibhav27 <cs1221301@iitd.ac.in>
2024-01-26 17:06:49 +0530

    Modularised Graph Plotter and added start and end time data construct in txt file for reference

--- daily_monthlyData.py
+++ daily_monthlyData.py
@@ -1,12 +1,11 @@
 import pandas as pd
+import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import date
 import numpy as np
 from jugaad_data.nse import stock_df
 
-symbol = "SBIN"
-years=5
 def save_df(symbol, last_years):
     """
         Inputs:
@@ -27,92 +26,84 @@ def save_df(symbol, last_years):
     df=pd.concat([df["DATE"], df[df.columns[1:]].astype("float32", copy=True)], axis=1)
     return df
 
-df1=save_df(symbol, years)
-df2=save_df("BHARTIARTL", 5)
-# print("DF1:")
-# print(df1["CLOSE"])
-# print(df1["DATE"])
-# print("DF2:")
-# print(df2["CLOSE"])
-# print(df2["DATE"])
-df=pd.DataFrame({
-    symbol:df1["CLOSE"].to_numpy(),
-}, index=df1["DATE"])
-df_=pd.DataFrame({
-    "BHARTIARTL":df2["CLOSE"].to_numpy()
-}, index=df2["DATE"])
-df = pd.merge(df, df_, left_index=True, right_index=True) # inner join, i.e. intersection of indices
-# print(df.head())
-# print("----------------")
-#df.set_index("DATE", inplace=True)
+def store_stocks(symbols, years):
+    # respective store dfs of respective symbols and years, where symbols and years are a list
+    stocksDF=[]
+    length = len(symbols)
+    for i in range(length):
+        df = save_df(symbols[i], years[i])
+        print("i:", i)
+        df = pd.DataFrame({
+            symbols[i]:df["CLOSE"].to_numpy()
+        }, index=df["DATE"])
+        stocksDF.append(df)
+    # Merging all based on close prices based on index i.e. DATE
+    df_merged=stocksDF[0]
+    for i in range(1, length):
+        df_merged = df_merged.merge(stocksDF[i], left_index=True, right_index=True)
+        
+    return df_merged
+        
+def plot_to_html(df_merged):
+    dfmon_merged = df_merged.resample('M').last()
+    dfyr_merged = df_merged.resample('AS').last()
+    
+    df_merged.index.names = ['Date']
+    df_merged.reset_index(inplace=True)
+    
+    dfmon_merged.index.names = ['Date']
+    dfmon_merged.reset_index(inplace=True)
+    
+    dfyr_merged.index.names = ['Date']
+    dfyr_merged.reset_index(inplace=True)
+    
+    fig = go.Figure()
+    dfs = {'daily':df_merged, 'monthly': dfmon_merged, 'yearly' :dfyr_merged}
+    
+    # specify visibility for traces accross dataframes
+    frames = len(dfs) # number of dataframes organized in  dict
+    columns = len(dfs['daily'].columns) - 1 # number of columns i df, minus 1 for Date
+    scenarios = [list(s) for s in [e==1 for e in np.eye(frames)]]
+    visibility = [list(np.repeat(e, columns)) for e in scenarios] 
+    
+    # container for buttons
+    buttons = []
 
-# print(df.head())
-# print("----------------")
-dfmon = df.resample('M').last()
-# print(dfmon.head())
-# print("----------------")
-dfyr = df.resample('AS').last()
-# print(dfyr.head())
-# print("----------------")
-
-df.index.names = ['Date']
-df.reset_index(inplace= True)
-
-dfmon.index.names = ['Date']
-dfmon.reset_index(inplace= True)
-
-dfyr.index.names = ['Date']
-dfyr.reset_index(inplace= True)
-# print(dfyr.head())
-fig = go.Figure()
-dfs = {'daily':df, 'monthly': dfmon, 'yearly' :dfyr}
-
-# # your setup this far...
-
-# # ... here is where I've added my contributions:
-
-# specify visibility for traces accross dataframes
-frames = len(dfs) # number of dataframes organized in  dict
-columns = len(dfs['daily'].columns) - 1 # number of columns i df, minus 1 for Date
-scenarios = [list(s) for s in [e==1 for e in np.eye(frames)]]
-visibility = [list(np.repeat(e, columns)) for e in scenarios] 
-
-# container for buttons
-buttons = []
-
-# iterate of dataframes in dfs:
-# - i is used to reference visibility attributes
-# - k is the name for each dataframe
-# - v is the dataframe itself
-for i, (k, v) in enumerate(dfs.items()):
-    print(i)
-    for c, column in enumerate(v.columns[1:]):
-        fig.add_scatter(name = column,
-                        x = v['Date'],
-                        y = v[column], 
-                        visible=True if k=='daily' else False # 'daily' values are shown from the start
-                       )
+    # iterate of dataframes in dfs:
+    # - i is used to reference visibility attributes
+    # - k is the name for each dataframe
+    # - v is the dataframe itself
+    for i, (k, v) in enumerate(dfs.items()):
+        print(i)
+        for c, column in enumerate(v.columns[1:]):
+            fig.add_scatter(name = column,
+                            x = v['Date'],
+                            y = v[column], 
+                            visible=True if k=='daily' else False # 'daily' values are shown from the   start
+                           )
                 
-    # one button per dataframe to trigger the visibility
-    # of all columns / traces for each dataframe
-    button =  dict(label=k,
-                   method = 'restyle',
-                   args = ['visible',visibility[i]])
-    buttons.append(button)
+        # one button per dataframe to trigger the visibility
+        # of all columns / traces for each dataframe
+        button =  dict(label=k,
+                       method = 'restyle',
+                       args = ['visible',visibility[i]])
+        buttons.append(button)
 
-# include dropdown updatemenu in layout
-fig.update_layout(updatemenus=[dict(type="dropdown",
-                                    direction="down",
-                                    buttons = buttons)])
-fig.update_layout(
-    dragmode="zoom",
-    hovermode="x",
-    legend=dict(traceorder="reversed"),
-    height=800,
-    template="plotly_white",
-    margin=dict(
-        t=100,
-        b=100
-    ),
-)
-fig.show()
+    # include dropdown updatemenu in layout
+    fig.update_layout(updatemenus=[dict(type="dropdown",
+                                        direction="down",
+                                        buttons = buttons)])
+    fig.update_layout(
+        dragmode="zoom",
+        hovermode="x",
+        legend=dict(traceorder="reversed"),
+        height=800,
+        template="plotly_white",
+        margin=dict(
+            t=100,
+            b=100
+        ),
+    )
+    
+    my_script = pio.to_html(fig, full_html=False)
+    return my_script
